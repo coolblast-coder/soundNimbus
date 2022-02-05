5fb0ff9bfa06a42b6afd09b5baa7f772c83dee47
coolblast-coder <lyapunovantik@gmail.com>
2022-02-05 14:43:38 -0500

    WIP - endpoints/routes - lyrics query by ID, about, music, 404

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 const path = require('path')
+const musicData = require('./musicData')
+
+
 const HTTP_PORT = process.env.port || 8080
 const onHttptart = () => console.log('HTTP server is listening on port ${HTTP_PORT} 🚀🚀🚀')
-    //app.get
-    //app.post
-    //app.use
 
 app.use(express.static('public'))
 
@@ -16,7 +16,47 @@ app.get('/', (req, res) => {
 
 app.get('/home', (req, res) => {
     // res.send('hello home')
+
     res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.send('hello about')
+})
+
+app.get('/lyrics', (req, res) => {
+    // res.send('hello lyrics')
+    musicData.getAlbums().then((data) => {
+        res.json(data)
+    }).catch((error) => {
+        console.log(error)
+        res.status(404).send("ERROR!")
+    })
+})
+
+app.get('/lyrics/:id', (req, res) => {
+    // res.send('hello lyrics')
+    musicData.getAlbums().then((data) => {
+        // res.json(data)
+        // res.send(req.params.id)
+
+        // resolved Promice Data [idFromRequestParams].field
+        res.json(data[req.params.id - 1].lyrics)
+
+
+    }).catch((error) => {
+        console.log(error)
+        res.status(404).send("ERROR!")
+    })
+})
+
+app.get('/music', (req, res) => {
+    res.send('hello muisic')
+})
+
+
+app.use((req, res) => {
+    res.status(404).send("Page not found!")
 })
 
 app.listen(HTTP_PORT, onHttptart);
